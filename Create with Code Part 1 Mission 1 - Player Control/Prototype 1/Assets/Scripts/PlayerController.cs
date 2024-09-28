d49f5d75637ddc5fca1c3ce2850d1328ed4954ca
Dedotated-Wham <lwong02@qub.ac.uk>
2024-09-28 20:52:17 +0100

    Added RestartGame Script
    Added new script to test.

--- Create with Code Part 1 Mission 1 - Player Control/Prototype 1/Assets/Scripts/PlayerController.cs
+++ Create with Code Part 1 Mission 1 - Player Control/Prototype 1/Assets/Scripts/PlayerController.cs
@@ -5,17 +5,17 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     //Private Variables
-    private float speed = 5.0f;
-    private float turnSpeed =25.0f;
+    private float speed = 20.0f;
+    private float turnSpeed = 30.0f;
     private float horizontalInput;
     private float forwardInput;
-   
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
-  
+
     // Update is called once per frame
     void Update()
     {
@@ -27,5 +27,9 @@ public class PlayerController : MonoBehaviour
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
         //We turn the vehicle
         transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+
+
+
     }
+
 }

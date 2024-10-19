1ff5dfb817a51d945f2a203e463d876ba019d960
Dedotated-Wham <lwong02@qub.ac.uk>
2024-10-19 22:51:12 +0100

    Challenge 2 - Patch 1.2
    Fixed issue with gameObjects not destroying when off screen.
    
    Dog - Changed the ">" to "<" resolved issue and increased leftLimit.
    
    Balls - Changed transform.position.z to y instead to resolve it.
    
    Bonus
    PlayerController - Added in cooldown to prevent spamming dogs.

--- Create with Code Part 1 Mission 2 - Basic Gameplay/Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Create with Code Part 1 Mission 2 - Basic Gameplay/Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -1,18 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+    private float cooldown = 1.0f;
 
     // Update is called once per frame
     void Update()
     {
-        // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        // On spacebar press and condition met for cooldown, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && cooldown <= 0)
         {
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            cooldown = 1.0f;
+        }
+        // Reset cooldown timer per frame
+        if (cooldown >= 0)
+        {
+            cooldown -= Time.deltaTime;
         }
     }
+    
 }
+

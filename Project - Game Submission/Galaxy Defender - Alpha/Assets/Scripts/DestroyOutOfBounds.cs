aebf50521363f02ae2e0e44230a853e34c2d9156
Dedotated-Wham <lwong02@qub.ac.uk>
2024-10-27 19:24:16 +0000

    Alpha - Update 1.1
    Created basic models for player object and environment obstacle tower.
    
    Created scripts for moving player and camera to follow behind player.
    
    Created DestroyOutOfBounds script and tested it to work.

--- Project - Game Submission/Galaxy Defender - Alpha/Assets/Scripts/DestroyOutOfBounds.cs
+++ Project - Game Submission/Galaxy Defender - Alpha/Assets/Scripts/DestroyOutOfBounds.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class DestroyOutOfBounds : MonoBehaviour
 {
+    public GameObject player;
+    private PlayerController playerControllerScript;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.z < (player.transform.position.z - 15.0f))
+        {
+            Destroy(gameObject);
+        }
+
+
+
     }
 }
+
--- Project - Game Submission/Galaxy Defender - Alpha/Assets/Scripts/PlayerController.cs
+++ Project - Game Submission/Galaxy Defender - Alpha/Assets/Scripts/PlayerController.cs
@@ -3,16 +3,71 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
+
 {
+    public GameObject player;
+    public float forwardSpeed = 5.0f;
+    public float turnSpeed = 100.0f;
+    public float verticalSpeed = 20.0f;
+    public float horizontalSpeed = 20.0f;
+
+
+    private float horizontalInput;
+    private float verticalInput;
+    public float rotateInput;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Move plane forward.
+        transform.Translate(Vector3.forward * Time.deltaTime * forwardSpeed);
+
+        //Controls to move player plane.
+
+        //Press 'W' or 'S' to move up or down.
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.up * verticalInput * Time.deltaTime * verticalSpeed, Space.World);
+
+        //Press 'A' or 'D' to move left or right.
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * horizontalSpeed, Space.World);
+
+        //Press 'A' or 'D' to rotate along forward axis.
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(Vector3.forward, Time.deltaTime * turnSpeed);
+
+        }
+
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.Rotate(Vector3.forward, Time.deltaTime * -turnSpeed);
+        }
+
+
+
         
+
+
+
+
+
+
+
+
     }
 }
+
+
+
+
+

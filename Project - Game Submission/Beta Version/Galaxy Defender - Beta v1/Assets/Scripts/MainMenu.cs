91d9a9f2260cf6e902efe86aaf20f1b76f0376c9
Dedotated-Wham <lwong02@qub.ac.uk>
2024-12-07 17:20:27 +0000

    Updated Main Menu Options.
    Updated Main Menu Options and added in extra buttons to improve on the UI.

--- Project - Game Submission/Beta Version/Galaxy Defender - Beta v1/Assets/Scripts/MainMenu.cs
+++ Project - Game Submission/Beta Version/Galaxy Defender - Beta v1/Assets/Scripts/MainMenu.cs
@@ -7,6 +7,21 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadSceneAsync(1); //Load scene index (1) which is Alpha Scene.
+        SceneManager.LoadSceneAsync(1); //Load scene index (1) which is Beta Scene.
+    }
+
+    public void HowToPlay()
+    {
+        SceneManager.LoadSceneAsync(2); //Load scene index (2) which is How To Play Menu.
+    }
+
+    public void Options()
+    {
+        SceneManager.LoadSceneAsync(3); //Load scene index (3) which is Options Menu.
+    }
+
+    public void ReturnToMenu()
+    {
+        SceneManager.LoadSceneAsync(0); //Load scene index (0) which is Main Menu.
     }
 }
